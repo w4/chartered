ecf8148080618cc52f47f74d3675a13a169d9701
Jordan Doyle <jordan@doyle.la>
2021-09-21 03:24:18 +0100

    Reorder migrations

--- migrations/2021-08-31-214501_create_crates_table/down.sql
+++ migrations/2021-08-31-214501_create_crates_table/down.sql
@@ -1,7 +1,7 @@
 DROP TABLE organisations;
+DROP TABLE users;
 DROP TABLE crates;
 DROP TABLE crate_versions;
-DROP TABLE users;
 DROP TABLE user_organisation_permissions;
 DROP TABLE user_crate_permissions;
 DROP TABLE user_ssh_keys;
