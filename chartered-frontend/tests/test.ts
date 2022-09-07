43770d20014b87b0bb7784b40b0a0871961fbc79
Jordan Doyle <jordan@doyle.la>
2022-09-07 02:35:57 +0100

    Add E2E UI tests in CI

--- chartered-frontend/tests/test.ts
+++ chartered-frontend/tests/test.ts
@@ -1,6 +1,37 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('redirect to login when unauthenticated', async ({ page }) => {
     await page.goto('/');
-    expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+    expect(await page.textContent('h1')).toBe('chartered ✈️');
+    expect(await page.textContent('button')).toBe('Login');
+});
+
+test('register button takes user to register page', async ({ page }) => {
+    await page.goto('/');
+    await page.locator('text=Register').click();
+    await expect(page).toHaveURL(/.*register/);
+    expect(await page.textContent('h1')).toBe('chartered ✈️');
+    expect(await page.textContent('button')).toBe('Register');
+});
+
+test('can successfully register and login', async ({ page }) => {
+    const username = Math.random().toString(36).substring(2,7);
+    const password = 'aaaaaaaa';
+
+    await page.goto('/');
+    await page.locator('text=Register').click();
+
+    await page.locator('input[id="username"]').fill(username);
+    await page.locator('input[id="password"]').fill(password);
+    await page.locator('text=Register').click();
+
+    await expect(page.locator('[role="alert"]')).toHaveCount(0);
+
+    await expect(page).toHaveURL(/.*login/);
+    await page.locator('input[id="username"]').fill(username);
+    await page.locator('input[id="password"]').fill(password);
+    await page.locator('text=Username Password Login >> button').click();
+
+    await expect(page).not.toHaveURL(/.*login/);
+    expect(await page.textContent('h1')).toBe('Welcome to Chartered.');
 });
