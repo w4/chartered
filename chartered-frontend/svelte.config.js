62a0597ad7878b94921faebd5997b3c5cf6ddaac
Jordan Doyle <jordan@doyle.la>
2022-09-08 22:44:26 +0100

    Move to @sveltejs/adapter-static for full SPA effect

--- chartered-frontend/svelte.config.js
+++ chartered-frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +10,11 @@ const config = {
     }),
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: 'index.html',
+        }),
         prerender: {
-            enabled: false,
+            entries: [],
         },
     },
 };
