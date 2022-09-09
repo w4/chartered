ec84c1dbc32507246104db20f0cf6c34767dae2f
Jordan Doyle <jordan@doyle.la>
2022-09-09 22:14:38 +0100

    Fix UUID types in Postgres migrations

--- migrations/postgres/2021-11-08-005634_update_sessions_add_uuid/up.sql
+++ migrations/postgres/2021-11-08-005634_update_sessions_add_uuid/up.sql
@@ -1,5 +1,5 @@
 -- drop all sessions before creating NOT NULL column
 DELETE FROM user_sessions;
 
-ALTER TABLE user_sessions ADD COLUMN uuid UUID NOT NULL;
+ALTER TABLE user_sessions ADD COLUMN uuid BYTEA NOT NULL;
 CREATE UNIQUE INDEX unique_user_sessions_uuid ON user_sessions(uuid);
